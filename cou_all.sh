a506fa9f9c9e05e469b39488ec2eeab710332bf1
Milenaverag <117789141+Milenaverag@users.noreply.github.com>
2022-11-18 18:45:26 -0500

    ejercicio 1.2

---
+++ cou_all.sh
@@ -0,0 +1,6 @@
+mIS=`tail -n +2 Gesquiere2011_data.csv | cut -f 1 | sort -n | uniq`
+for id in $mIS
+do
+    counts=`bash count_baboons.sh Gesquiere2011_data.csv $id`
+    echo "ID:" $id "counts:" $counts
+done
---
+++ count_baboons.sh
@@ -0,0 +1,2 @@
+cut -f 1 Gesquiere2011_data.csv |  grep -c -w  1
+
